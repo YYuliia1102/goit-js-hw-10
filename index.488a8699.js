e52de28f18ecdef0e169a25f63e40c42115fc125
YYuliia1102 <117763038+YYuliia1102@users.noreply.github.com>
2023-05-15 21:00:06 +0000

    Deploying to gh-pages from @ YYuliia1102/goit-js-hw-10@2132098f830ee881874c625ab3a5d6f3f72d28a0 🚀

--- index.488a8699.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={fetchCountries:function(t){return fetch("".concat("https://restcountries.com/v3.1/name/").concat(t)).then((function(t){return t.json()}))}};document.querySelector("#search-box").addEventListener("keyup",(function(n){var e=n.currentTarget.value.trim();t.fetchCountries(e).then((function(t){return console.log(t)}))}))}();
-//# sourceMappingURL=index.488a8699.js.map
--- index.49c640c4.js
+++
@@ -1,2 +0,0 @@
-var e={fetchCountries:function(e){return fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>e.json()))}};document.querySelector("#search-box").addEventListener("keyup",(function(t){const n=t.currentTarget.value.trim();e.fetchCountries(n).then((e=>console.log(e)))}));
-//# sourceMappingURL=index.49c640c4.js.map
