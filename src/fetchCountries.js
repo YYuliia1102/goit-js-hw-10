0789df0d4ad6b5a149b0b7506075741e1da5c46d
YYuliia1102 <reya1102@gmail.com>
2023-05-16 17:59:28 +0300

    fixed bags

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,12 @@
 import './css/styles.css';
 const URL = "https://restcountries.com/v3.1/name/"
 
+
 // https://restcountries.com/v3.1/name/{name}
 
 
 function fetchCountries(name) {
-    return fetch(`${URL}${name}`).then((res) =>
+    return fetch(`${URL}${name}?fields=name,flags,languages,capital,population`).then((res) =>
         res.json()
     )
 }
